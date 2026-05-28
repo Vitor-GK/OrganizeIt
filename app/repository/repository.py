eb7894539ebef6e123286aa719877868962b8f7d
Vitor Glier Kockhann <vitorgkockhann@gmail.com>
2026-05-28 16:55:22 -0300

    initial logic for register an user

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,5 +1,24 @@
 from sqlalchemy.orm import Session
+from pydantic import EmailStr
+from models.models import User
+from schemas.user import UserRegister
+from enums import RoleEnum
 
 class Repository:
     def __init__(self, db: Session):
         self.db = db
+
+    def get_email(self, email: EmailStr):
+        return self.db.query(User).filter(User.email == email).first()
+
+    def register_user(self, user_register: UserRegister):
+        new_user = User(full_name=user_register.full_name,
+                        email=user_register.email,
+                        birth_date=user_register.birth_date,
+                        password=user_register.password,
+                        role=RoleEnum.GUEST
+        )
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
--- app/service/service.py
+++ app/service/service.py
@@ -1,5 +1,13 @@
 from repository.repository import Repository
+from schemas.user import UserRegister
+from fastapi import HTTPException
 
 class Service:
     def __init__(self, repo: Repository):
         self.repo = repo
+
+    def register_user(self, user_register: UserRegister):
+        user = self.repo.get_email(user_register.email)
+        if user:
+            raise HTTPException(status_code=409, detail="Email already registered")
+        return self.repo.register_user(user_register)
